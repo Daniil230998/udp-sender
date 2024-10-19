35fbefd39e7f2d6375008b53edc7f56cbb264a7f
Daniil <danya.pozdnyakov.98@mail.ru>
2024-10-19 16:47:41 +0300

    upfate udp_sender

--- cpp/udp_client.cpp
+++ cpp/udp_client.cpp
@@ -7,11 +7,6 @@
 #include <string>
 #include "message.pb.h"
 #include <vector>
-//#include "overtaking.hpp"
-//struct Point{
-//  float x;
-//  float y;
-//};
 
 
 
@@ -20,31 +15,36 @@ int main(){
     boost::asio::io_service service;
     boost::asio::ip::udp::socket sock(service);
 
-    //boost::asio::ip::udp::resolver resolver(service);
-    //boost::asio::ip::udp::resolver::query query("localhost", "3380");
-    //boost::asio::ip::udp::resolver::iterator iter = resolver.resolve(query);
-
+    boost::asio::ip::udp::resolver resolver(service);
+    boost::asio::ip::udp::resolver::query query("127.0.0.1", "3380");
+    boost::asio::ip::udp::resolver::iterator iter = resolver.resolve(query);
+    //Открытие сокета
     sock.open(boost::asio::ip::udp::v4());
     boost::asio::ip::udp::endpoint sender_ep;
     boost::system::error_code err;
-    sock.bind(boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string("192.168.1.67"), 3380));//*iter
+    //Назначение устройства куда нужно отправить сообщение
+    sock.bind(boost::asio::ip::udp::endpoint(*iter));
     char *a2[1330];
     message::Message2 m;
-    int size;//=133;
-    //std::vector<Point> b;
-
+    int size;
+    // Цикл для принятия двух сообщений
+    for(j=0;j<2;j=j+1){
+    //Принятие сообщения
     size=sock.receive_from(boost::asio::buffer(a2), sender_ep,0,err);
-    
+    //Извлечение сообщения из буфера
     m.ParseFromArray(&a2, size);
-    //b=overtaking(m);
-    //for(i=0;i<b.size();i=i+2){
-    //        std::cout<<"i="<<i<<"\n";
-    // std::cout<<"b.x="<<b[i].x<<" b.y="<<b[i].y<<"\n";
-    // std::cout<<"b.x="<<b[i+1].x<<" b.y="<<b[i+1].y<<"\n";
-    //}
-    std::cout<<"size="<<size<<"\n";
+    //Цикл для вывода
     for(i=0;i<m.car().size();i=i+1){
     std::cout<<"id="<<m.car(i).id()<<" speed="<<m.car(i).speed()<<" x="<<m.car(i).x()<<" y="<<m.car(i).y()<<" w="<<m.car(i).w()<<" l="<<m.car(i).l()<<std::endl;
+    }
+    std::cout<<"\n";
+    //Цикл очистки буфера
+    for(i1=0;i1<5;i1=i1+1){
+       a2[i1]=0;
+    }
+    m.Clear();
+    size=0;
+    sleep(1);
     }
     sock.close();
 }
--- cpp/udp_server.cpp
+++ cpp/udp_server.cpp
@@ -6,29 +6,33 @@
 #include <iostream>
 #include "message.pb.h"
 #include <string>
-//#include <boost/lexical_cast.hpp>
-//#include <google/protobuf/message_lite.h>
-//using namespace google::protobuf;
 
 int main(){
     boost::asio::io_service service;
     boost::asio::ip::udp::socket sock(service);
 
-    //boost::asio::ip::udp::resolver resolver(service);
-    //boost::asio::ip::udp::resolver::query query("localhost", "3380");
-   // boost::asio::ip::udp::resolver::iterator iter = resolver.resolve( query);
+    boost::asio::ip::udp::resolver resolver(service);
+    boost::asio::ip::udp::resolver::query query("127.0.0.1", "3380");
+    boost::asio::ip::udp::resolver::iterator iter = resolver.resolve( query);
+    
     int size=0,i=0,j=0,i1=0;
     std::size_t size1;
+    //Открытие сокета
     sock.open(boost::asio::ip::udp::v4());
-    boost::asio::ip::udp::endpoint receiver_ep=boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string("192.168.1.67"), 3380);//*iter
+    //Назначение устройства куда нужно отправить сообщение
+    boost::asio::ip::udp::endpoint receiver_ep=boost::asio::ip::udp::endpoint(*iter);
     boost::system::error_code err;
     message::Message2 m;
     char *buf1[58];
     message::Message *m1;
+    //Создание дыух массивов, которые нужно передать
     int id[5]={0,1,2,3,4};
     float s[5]={16.0,16.0,22.22,19.44,16.0},x[5]={5.0,-2.5,-2.5,0.0,0.0},y[5]={10.0,21.0,25.0,30.0,-21.0},w[5]={1.6,1.7,1.7,1.7,1.7},l[5]={4.2,4.25,4.3,4.2,4.3};
-    //int id[5]={0,1,2,3};
-    //float s[5]={16.0,16.0,16.0,16.44},x[5]={4.0,-1.0,0.0,0.0},y[5]={4.0,21.0,-21.0,5.0},w[5]={1.6,1.7,1.7,1.7},l[5]={4.2,4.25,4.2,4.2};
+    int id1[5]={0,1,2,3};
+    float s1[5]={16.0,16.0,16.0,16.44},x1[5]={4.0,-1.0,0.0,0.0},y1[5]={4.0,21.0,-21.0,5.0},w1[5]={1.6,1.7,1.7,1.7},l1[5]={4.2,4.25,4.2,4.2};
+    //Основной цикл отправки сообщение
+    for(j=0;j<2;j=j+1){
+    //Цикл для задания данных в сообщении
     for(i=0;i<5;i=i+1){
     message::Message *m1=m.add_car();
     m1->set_id(id[i]);
@@ -38,17 +42,38 @@ int main(){
     m1->set_w(w[i]);
     m1->set_l(l[i]);
     }
+    //Размер полученного сообщения в байтах
     size1=m.ByteSizeLong();
     i1=0;
-    m.SerializeToArray(&buf1, size1);//5000
+    //записьс полученного массива в буфер для передачи
+    m.SerializeToArray(&buf1, size1);
+    //Отправка сообщения
     sock.send_to(boost::asio::buffer(buf1), receiver_ep,0,err);
+    //Цикл обнуления данных
+    for(i1=0;i1<5;i1=i1+1){
+       id[i1]=0;
+       s[i1]=0;
+       x[i1]=0;
+       y[i1]=0;
+       w[i1]=0;
+       l[i1]=0;
+    }
+    //Цикл для записи второго пакета для передачи
+    for(i1=0;i1<4;i1=i1+1){
+       id[i1]=id1[i1];
+       s[i1]=s1[i1];
+       x[i1]=x1[i1];
+       y[i1]=y1[i1];
+       w[i1]=w1[i1];
+       l[i1]=l1[i1];
+    }
+    //Очистка классов
+    m1->Clear();
+    m.Clear();
+    sleep(1);
+    }
+    //Закрытие сокета
     sock.close();
-    //std::cout<<"sizeof="<<size1<<"\n";
-    //std::cout<<m.car(0).id()<<"\n";
-    //std::cout<<m.car(0).speed()<<"\n";
-    //std::cout<<m.car(0).x()<<"\n";
-    //std::cout<<m.car(0).y()<<"\n";
-    //std::cout<<m.car(0).w()<<"\n";
-    //std::cout<<m.car(0).l()<<"\n";
+    //Уничтожение объекта класса protobuf   
     google::protobuf::ShutdownProtobufLibrary();
 }
